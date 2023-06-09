506d776c3fd8136663031beb0fc6a2857e4c631e
罗培淮 <luopeihuai@datahunter.cn>
2023-06-09 18:10:11 +0800

    int

--- internal/model/tbl_column.go
+++ internal/model/tbl_column.go
@@ -54,7 +54,7 @@ func (c *Column) ToField(nullable, coverable, signable, softDelete bool) *Field 
 		fieldType = "u" + fieldType
 	}
 	switch {
-	case softDelete && c.Name() == "deleted_time" && fieldType == "int64":
+	case softDelete && c.Name() == "deleted_time" && (fieldType == "int64" || fieldType == "int"):
 		fieldType = "field_type.DeletedTime"
 	case c.Name() == "deleted_at" && fieldType == "time.Time":
 		fieldType = "gorm.DeletedAt"
